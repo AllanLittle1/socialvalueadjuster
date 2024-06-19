558d12aa6b5fc75ed9467457f22d12b09fab109a
Allan Little <allanmlittle@hotmail.com>
2024-06-19 23:26:21 +0100

    CY/FY set up at top and working. Get Real button added. Lots of styling

--- server.R
+++ server.R
@@ -1,38 +1,38 @@
+# LOAD LIBRARIES --------------
 library(shiny)
-library(bslib)
-
-# Load the deflators data -----------
-deflators <- read_excel("socialvalueadjuster_deflators.xlsx", sheet = "deflators_cy")
+library(readxl)
 
 # Define Server
 server <- function(input, output, session) {
+  # Load the deflators data inside the server function
+  deflators <- read_excel("socialvalueadjuster_deflators.xlsx", sheet = "deflators")
+  
   observeEvent(input$adjust, {
     req(input$nominal_value, input$price_year, input$adjusted_year)  # Ensure inputs are available
     
-    # Ensure inputs are numeric
     nominal_value <- as.numeric(input$nominal_value)
     price_year <- as.numeric(input$price_year)
     adjusted_year <- as.numeric(input$adjusted_year)
     
-    if (is.na(nominal_value) || is.na(price_year) || is.na(adjusted_year)) {
-      output$adjusted_value <- renderText("Please enter valid numeric values.")
+    if (input$year_type == "fy") {
+      # Financial Year
+      deflator_start <- deflators$deflator_fy[deflators$year == price_year]
+      deflator_end <- deflators$deflator_fy[deflators$year == adjusted_year]
     } else {
-      # Find the deflators for the specified years
+      # Calendar Year
       deflator_start <- deflators$deflator_cy[deflators$year == price_year]
       deflator_end <- deflators$deflator_cy[deflators$year == adjusted_year]
+    }
+    
+    if (is.na(nominal_value) || is.na(deflator_start) || is.na(deflator_end)) {
+      output$adjusted_value <- renderText("Please enter valid numeric values.")
+    } else {
+      # Adjust the nominal value using the deflators
+      adjusted_value <- nominal_value * (deflator_end / deflator_start)
       
-      # Check if deflators are available
-      if (length(deflator_start) == 0 || length(deflator_end) == 0) {
-        output$adjusted_value <- renderText("Deflators for the selected years are not available.")
-      } else {
-        # Adjust the nominal value using the deflators
-        adjusted_value <- nominal_value * (deflator_end / deflator_start)
-        
-        output$adjusted_value <- renderText({
-          paste("£", round(adjusted_value, 2))
-        })
-      }
+      output$adjusted_value <- renderUI({
+        HTML(paste0("<div style='text-align: center; font-size: 18px;'>Your value in real terms is <b style='color: #337ab7;'>£", round(adjusted_value, 2), "</b>.</div>"))
+      })
     }
   })
 }
-
--- ui.R
+++ ui.R
@@ -2,7 +2,6 @@
 library(shiny)
 library(shinyjs)
 library(shinyWidgets)
-library(shinyBS)
 library(readxl)
 library(shinythemes)
 library(DT)
@@ -10,46 +9,54 @@ library(dplyr)
 library(httr)
 library(plotly)
 library(bslib)
-
-# Load the deflators data ---------
-
-deflators <- read_excel("socialvalueadjuster_deflators.xlsx", sheet = "deflators_cy")
+library(bsicons)
 
 # Define UI -------
-
 ui <- navbarPage(
-  theme = bs_theme(bootswatch = "flatly"),  # Apply the darkly theme
-  "Real Social Value App",
+  theme = bs_theme(bootswatch = "flatly"),
+  "Get Real",
   
   tabPanel("Welcome",
            fluidPage(
              useShinyjs(),
-             titlePanel("Welcome to the Real Social Value App"),
+             titlePanel("Welcome to Get Real."),
              mainPanel(
                h3("Introduction"),
-               p("This tool helps you adjust your social value estimates into real prices and values 
-                 - a crucial step in creating Treasury-compliant social impact analysis."),
-               p("Use the 'Inflation Adjuster' tab to start adjusting values.")
+               p("This tool helps you adjust your social values into real prices and values 
+                 - a crucial step to make your estimates into Treasury-compliant, Business Case ready analysis."),
+               p("Get Real: adjusts for inflation to express your values in real terms. 
+                  Get Present: to apply Treasury discount rates, expressing your estimates in Present Values
+                  Get Ready: to access our Social Value checklist. Self-rate your analysis, download a report.
+                  Get AI: xyz")
              ))),
   
-  tabPanel("Inflation Adjuster",
-    fluidRow(
-      column(6, offset = 3,
-        div(class = "well",
-          div(style = "display: flex; align-items: center; flex-wrap: wrap; font-size: 18px;",
-            span(style = "padding-right: 5px;", "My social value is £"),
-            numericInput("nominal_value", "", value = 10, min = 0, step = 0.01, width = '100px'),
-            span(style = "padding: 0 5px;", "in"),
-            numericInput("price_year", "", value = 2020, min = 2000, max = 2028, width = '100px'),
-            span(style = "padding: 0 5px;", "prices.")),
-          div(style = "display: flex; align-items: center; flex-wrap: wrap; font-size: 18px;",
-            span(style = "padding-right: 5px;", "What would this be in real terms, based on"),
-            numericInput("adjusted_year", "", value = 2023, min = 2000, max = 2028, width = '100px'),
-            span(style = "padding-left: 5px;", "prices?")),
-          br(),
-          actionButton("adjust", "Show Amount"),
-          h4("Value"),
-          verbatimTextOutput("adjusted_value")
-        ))))
-  
+  tabPanel("Get Real: Inflation Adjuster",
+           fluidPage(
+             fluidRow(
+               column(6, offset = 3,
+                      div(class = "well",
+                          div(style = "display: flex; align-items: center; flex-wrap: wrap; font-size: 18px;",
+                              prettyRadioButtons(inputId = "year_type", label = " ", choices = c("Calendar years" = "cy", "Financial years" = "fy"),
+                                                 inline = TRUE, icon = icon("check"), bigger = TRUE, status = "info", animation = "jelly"),
+                              tooltip(bsicons::bs_icon("info-circle"), "For Financial Year, the input year corresponds to the start of the FY. E.g., 2022 means FY 2022/23", id = "year_type_tooltip")),
+                          div(style = "display: flex; align-items: center; flex-wrap: wrap; font-size: 18px;",
+                              span(style = "padding-right: 5px;", "My social value is £"),
+                              numericInput("nominal_value", "", value = 10, min = 0, step = 0.01, width = '100px'),
+                              span(style = "padding: 0 5px;", "in"),
+                              numericInput("price_year", "", value = 2020, min = 2000, max = 2028, width = '100px'),
+                              span(style = "padding: 0 5px;", "prices.")),
+                          br(),
+                          div(style = "display: flex; align-items: center; flex-wrap: wrap; font-size: 18px;",
+                              span(style = "padding-right: 5px;", "What would this be in real terms, based on"),
+                              numericInput("adjusted_year", "", value = 2023, min = 2000, max = 2028, width = '100px'),
+                              span(style = "padding-left: 5px;", "prices?")),
+                          br(),
+                          actionBttn(inputId = "adjust", label = "Get Real", style = "unite", color = "primary"),
+                          br(),br(),
+                          htmlOutput("adjusted_value")
+                      )
+               )
+             )
+           )
+  )
 )
